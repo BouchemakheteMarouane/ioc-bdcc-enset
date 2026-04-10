b1ebb2d9dd3f71dc8f2b96c8f66995e16acc03b0
mapyah <marouane.bouch0@gmail.com>
2026-04-10 17:37:19 +0100

    Initial commit

---
+++ src/main/java/net/Bouchemakhete/Main.java
@@ -0,0 +1,13 @@
+package net.Bouchemakhete;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println("Vous avez tapé : " + input);
+    }
+}
